488c89a3ed4dfd1323c66560eeba40550841ef9e
zouzhigang <zouzhigang@loopeer.com>
2016-04-18 17:49:06 +0800

    Initial Commit

---
+++ ZZGViewPager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZZGViewPager
+//
+//  Created by dito on 16/4/18.
+//  Copyright © 2016年 zouzhigang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZZGViewPagerTests/ZZGViewPagerTests.m
@@ -0,0 +1,39 @@
+//
+//  ZZGViewPagerTests.m
+//  ZZGViewPagerTests
+//
+//  Created by dito on 16/4/18.
+//  Copyright © 2016年 zouzhigang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ZZGViewPagerTests : XCTestCase
+
+@end
+
+@implementation ZZGViewPagerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
